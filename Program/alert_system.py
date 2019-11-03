4159beb12801f87e5ea546cfc1603e47658c22b4
Norbu Sonam <nsonam1822@gmail.com>
2019-11-03 14:51:33 -0500

    removed twilio info

--- Program/alert_system.py
+++ Program/alert_system.py
@@ -2,40 +2,40 @@ from twilio.rest import Client
 
 def send_alert(location):
 
-    account_sid = "AC06cf139374327be6c8401c39afacb507"
-    auth_token = "641dddfb20d8d4069bebc652d0ab46ec"
+    account_sid = "SID"
+    auth_token = "AUTH TOKEN"
 
     client = Client(account_sid, auth_token)
     
     client.messages.create(
-        to = "+15082809733",
-        from_ = "+17636007661",
+        to = "NUM",
+        from_ = "NUM",
         body = "(UNCONFIRMED) Gunfire detected in {}.".format(location)
     )
 
 def send_alert_confirmation(location):
 
-    account_sid = "AC06cf139374327be6c8401c39afacb507"
-    auth_token = "641dddfb20d8d4069bebc652d0ab46ec"
+    account_sid = "SID"
+    auth_token = "AUTH TOKEN"
 
     client = Client(account_sid, auth_token)
     
     client.messages.create(
-        to = "+15082809733",
-        from_ = "+17636007661",
+        to = "NUM",
+        from_ = "NUM",
         body = "(CONFIRMED) Gunfire detected in {}.".format(location)
     )
 
 def send_false_alarm():
 
-    account_sid = "AC06cf139374327be6c8401c39afacb507"
-    auth_token = "641dddfb20d8d4069bebc652d0ab46ec"
+    account_sid = "SID"
+    auth_token = "AUTH TOKEN"
 
     client = Client(account_sid, auth_token)
     
     client.messages.create(
-        to = "+15082809733",
-        from_ = "+17636007661",
+        to = "NUM",
+        from_ = "NUM",
         body = "(FALSE ALARM) Previous message was a false alarm."
     )
 
